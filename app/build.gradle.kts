e29e9d1608d61fc6d669e81082eddf244cecb999
DanielX8 <103602507+DanielX8@users.noreply.github.com>
2026-05-28 12:10:17 +0300

    Rename application package to com.pesasense.xmqs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
   compileSdk { version = release(36) { minorApiLevel = 1 } }
 
   defaultConfig {
-    applicationId = "com.aistudio.pesasense.xmqs"
+    applicationId = "com.pesasense.xmqs"
     minSdk = 24
     targetSdk = 36
     versionCode = 1
