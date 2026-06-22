d80438dff5c7514852a4aa845e2d00ff003e1be4
DanielX8 <dleinx8@gmail.com>
2026-06-22 14:12:09 +0300

    chore: bump to v1.4.1, add CI release workflow, update changelog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
     applicationId = "com.pesalytics"
     minSdk = 24
     targetSdk = 36
-    versionCode = 3
-    versionName = "1.4.0"
+    versionCode = 4
+    versionName = "1.4.1"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
