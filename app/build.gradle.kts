f062466e0d54e000d132cd84a0c5d5fb8d54a47b
DanielX8 <dleinx8@gmail.com>
2026-09-03 12:06:57 +0300

    chore: bump version to 1.6.1 for hotfix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
     applicationId = "com.pesalytics"
     minSdk = 23
     targetSdk = 36
-    versionCode = 23
-    versionName = "1.6.0"
+    versionCode = 24
+    versionName = "1.6.1"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
